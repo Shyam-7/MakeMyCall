767db194a173c19c09aadbbd10924251a9b1f46c
Shyam-7 <justsgk07@gmail.com>
2026-06-26 23:36:50 +0530

    fix: update base path in vite config to root directory

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MakeMyCall/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
