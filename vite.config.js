b067b3f7c8703814feecb28c547c517a137bd0fa
Shyam-7 <justsgk07@gmail.com>
2026-06-27 15:54:47 +0530

    feat: add vite-plugin-sitemap for improved SEO and site mapping

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://www.makemycall.co.in',
+    })
+  ],
 })
